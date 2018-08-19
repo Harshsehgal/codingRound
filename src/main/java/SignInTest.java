a5e52fb13be4112fdaec853e92482fd9ee301a11
Sehgal, Harsh <HarshSehgal@qainfotech.com>
2018-08-19 20:41:28 +0530

    Added logging for complete understanding of test execution

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+@SuppressWarnings("restriction")
 public class SignInTest {
 
     WebDriver driver = new ChromeDriver();
@@ -14,15 +16,22 @@ public class SignInTest {
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         setDriverPath();
-
+        
+        // For better visibility of browser instance created by WebDriver
+        driver.manage().window().maximize();
+        
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
-
+        
         driver.findElement(By.linkText("Your trips")).click();
+        Reporter.log("Clicked on 'Your trips' link", true);
+        
         driver.findElement(By.id("SignIn")).click();
-
+        Reporter.log("Clicked on 'Sign In' button", true);
+        
         driver.findElement(By.id("signInButton")).click();
-
+        Reporter.log("Clicked on 'Sign in' button from SignIn modal window", true);
+        
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
@@ -36,7 +45,6 @@ public class SignInTest {
         }
     }
 
-    @SuppressWarnings("restriction")
 	private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
